124900a777dc70828847afaae49be408c53ad5d1
Lazybaxy <halvard.erla@gmail.com>
2021-06-07 22:59:38 +0200

    better resp messages

--- gitclone.bat
+++ gitclone.bat
@@ -1,2 +1,7 @@
 @echo off
-if [%1]==[] (echo no args) ELSE (git clone https://github.com/h3rl/%1.git)
+if [%1]==[] (
+	echo No parameters have been provided.
+	echo Provide repo ^(type string^)
+) ELSE (
+	git clone https://github.com/h3rl/%1.git
+)
--- gitcommit.bat
+++ gitcommit.bat
@@ -1,5 +1,8 @@
 @echo off
-if [%1]==[] (echo need commitmessage as arg..) ELSE (
+if [%1]==[] (
+	echo No parameters have been provided.
+	echo Provide commit message ^(type string^)
+) ELSE (
 	git add -A
 	git commit -a -m %1
 	git push
